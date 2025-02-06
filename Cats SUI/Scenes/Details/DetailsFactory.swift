cad46bc231b03ed2c88a87c5ff87ecd635a83847
evenstaian <evenstaian@gmail.com>
2025-02-06 13:13:03 -0300

    create Scenes and factories

---
+++ Cats SUI/Scenes/Details/DetailsFactory.swift
@@ -0,0 +1,16 @@
+//
+//  DetailsFactory.swift
+//  Cats SUI
+//
+//  Created by Evens Taian on 06/02/25.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+enum DetailsFactory {
+    static func makeModule() -> UIViewController {
+        return UIViewController()
+    }
+}
---
+++ Cats SUI/Scenes/Feed/FeedFactory.swift
@@ -0,0 +1,16 @@
+//
+//  FeedFactory.swift
+//  Cats SUI
+//
+//  Created by Evens Taian on 06/02/25.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+enum FeedFactory {
+    static func makeModule() -> UIViewController {
+        return UIViewController()
+    }
+}
