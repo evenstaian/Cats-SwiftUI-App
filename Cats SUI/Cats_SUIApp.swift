c3d32ebacd1971bbae12cfb2c20a47433e5c751c
evenstaian <evenstaian@gmail.com>
2025-02-06 12:50:26 -0300

    Initial Commit

---
+++ Cats SUI/Cats_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cats_SUIApp.swift
+//  Cats SUI
+//
+//  Created by Evens Taian on 06/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Cats_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cats SUITests/Cats_SUITests.swift
@@ -0,0 +1,16 @@
+//
+//  Cats_SUITests.swift
+//  Cats SUITests
+//
+//  Created by Evens Taian on 06/02/25.
+//
+
+import Testing
+
+struct Cats_SUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
